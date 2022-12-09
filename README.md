90b62e39b12462bc5d61908167f7145c47f2b4b1
CrimsonDawn45 <gannonburks45@gmail.com>
2022-12-09 14:51:00 -0600

    Started updating to 1.19

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Fabric-Shield-Lib-Example-Mod
+# FSL Example-Mod
+
+## Setup
+
+For setup instructions please see the [fabric wiki page](https://fabricmc.net/wiki/tutorial:setup) that relates to the IDE that you are using.
+
+## FSL Setup
 This is an example mod repo for [Fabric Shield Lib](https://github.com/CrimsonDawn45/Fabric-Shield-Lib).
 To quickly create a mod that uses it you can just use this template.
 
@@ -9,3 +15,7 @@ There is also a tutorial available at the [Fabric Wiki](https://fabricmc.net/wik
 
 
 ![Tutorial Steps 2-3](https://i.imgur.com/BD7nd10.png)
+
+## License
+
+This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,42 +1,14 @@
 package net.fabricmc.example;
 
-import com.github.crimsondawn45.fabricshieldlib.lib.event.ShieldBlockCallback;
-import com.github.crimsondawn45.fabricshieldlib.lib.event.ShieldDisabledCallback;
-import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricBannerShieldItem;
-import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShieldEnchantment;
-import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShieldItem;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.enchantment.Enchantment.Rarity;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.Items;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-
-	public static final String MOD_ID = "examplemod";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-
-	public static final Item EXAMPLE_SHIELD = new FabricShieldItem(new FabricItemSettings().maxDamage(2500).group(ItemGroup.COMBAT), 100, 9, Items.OAK_PLANKS); // FabricShieldItem(settings.maxDamage(durability), cooldownTicks, enchantability, repairItem)
-
-	public static final Item EXAMPLE_BANNER_SHIELD = new FabricBannerShieldItem(new FabricItemSettings().maxDamage(2500).group(ItemGroup.COMBAT), 100, 9, Items.OAK_PLANKS); // FabricBannerShieldItem(settings.maxDamage(durability), cooldownTicks, enchantability, repairItem)
-
-	public static final FabricShieldEnchantment EXAMPLE_ENCHANTMENT = new FabricShieldEnchantment(Rarity.COMMON);
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
 	@Override
 	public void onInitialize() {
@@ -44,39 +16,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "example_shield"), EXAMPLE_SHIELD);
-
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "example_banner_shield"), EXAMPLE_BANNER_SHIELD);
-
-		Registry.register(Registry.ENCHANTMENT, new Identifier(MOD_ID, "example_enchantment"), EXAMPLE_ENCHANTMENT);
-
-		/*
-		 * Test event: makes any shield with new enchantment reflect 1/3rd of damage back to attacker
-		 */
-		ShieldBlockCallback.EVENT.register((defender, source, amount, hand, shield) -> {
-
-			if(EXAMPLE_ENCHANTMENT.hasEnchantment(shield)) {
-				Entity attacker = source.getAttacker();
-
-				assert attacker != null;
-				if(defender instanceof PlayerEntity) {  //Defender should always be a player, but check anyways
-					attacker.damage(DamageSource.player((PlayerEntity) defender), (int)Math.round(amount * 0.33F));
-				} else {
-					attacker.damage(DamageSource.mob(defender), (int)Math.round(amount * 0.33F));
-				}
-			}
-
-			return ActionResult.PASS;
-		});
-
-		/*
-		 * Test Event: if your shield gets disabled give player speed
-		 */
-		ShieldDisabledCallback.EVENT.register((defender, hand, shield) -> {
-			defender.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 10, 1, true, false));
-			return ActionResult.PASS;
-		});
-
-		LOGGER.info(MOD_ID + " initialized.");
+		LOGGER.info("Hello Fabric world!");
 	}
 }
